3941730320094d772d09beebc5428b505eb26562
sharklaar <marc@blackhawkdown.org.uk>
2013-09-30 22:07:17 +0100

    First commit

---
+++ BhdResponsiveSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BhdResponsiveSite.MvcApplication" Language="C#" %>
---
+++ BhdResponsiveSite/Helpers/ContentHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BhdResponsiveSite.Helpers
+{
+    public class ContentHelper
+    {
+        public void GetContentFromGoogleDrive()
+        {
+
+        }
+
+        public void WriteContentToTextFile()
+        {
+
+        }
+
+        public void GetContentFromTextFile()
+        {
+
+        }
+
+
+    }
+}

9bea5342c630e8a405ffbcae1ade102d82a00d82
Marc <marc@blackhawkdown.org.uk>
2014-01-01 23:53:41 +0000

    added animation to read more bits

--- BhdResponsiveSite/Scripts/custom/about-page.js
+++ BhdResponsiveSite/Scripts/custom/about-page.js
@@ -3,15 +3,23 @@
         var section = '.' + event.target.id;
 
         if ($(this).hasClass('closed')) {
-            $(section).height('100%');
+            $(section).animate({
+                height: "100%"
+            });
+
             $(this).text("READ LESS...");
             $(this).removeClass('closed');
             $(this).addClass('open');
         } else {
-            $(section).height('142px');
+            $(section).animate({
+                height: '142px'
+            });
+
             $(this).text("READ MORE...");
             $(this).removeClass('open');
             $(this).addClass('closed');
         }
+
+        event.preventDefault();
     })
 })

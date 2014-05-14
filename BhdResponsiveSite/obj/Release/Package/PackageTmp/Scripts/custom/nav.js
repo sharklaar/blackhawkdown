a3006993f542c27586f1c6e7cac925617ad42c83
unknown <mdaffern@PEG-PC-007563.insurance.financial.local>
2014-05-14 13:35:18 +0100

    newtonsoft shenanigans and some tweaks to some stuff

---
+++ BhdResponsiveSite/obj/Release/Package/PackageTmp/Scripts/custom/nav.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#globalNavButton').click(function () {
+        var nav = $('#navigation');
+        if (!nav.hasClass('open')) {
+            nav.slideDown('fast');
+            nav.addClass('open');
+        } else {
+            nav.slideUp('fast');
+            nav.removeClass('open');
+        }                
+    })
+})

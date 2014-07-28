658cca0ccd47674041f3af9a71a0b01328df896b
Marc <marc@blackhawkdown.org.uk>
2014-07-28 23:41:02 +0100

    added members area and login shizznit

---
+++ BhdResponsiveSite/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using BhdResponsiveSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BhdResponsiveSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login(AccountModel account)
+        {
+
+            return View("Members", account);
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+    }
+}

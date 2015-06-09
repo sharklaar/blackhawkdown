7fda37fb1248902ac045acbeac1efd443652df4e
unknown <mdaffern@PEG-PC-007563.insurance.financial.local>
2015-06-09 07:43:45 +0100

    added email campaign test button

---
+++ BhdResponsiveSite/Models/EmailSendingResultsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BhdResponsiveSite.Models
+{
+    public class EmailSendingResultsModel
+    {
+        public int Successes { get; set; }
+        public int Failures { get; set; }
+    }
+}

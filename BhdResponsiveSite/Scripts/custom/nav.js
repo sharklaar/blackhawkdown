fac0a0558aa597222fa18f69b51c61cf2a102439
Marc <marc@blackhawkdown.org.uk>
2015-06-10 22:17:07 +0100

    made nav nicer

--- BhdResponsiveSite/Scripts/custom/nav.js
+++ BhdResponsiveSite/Scripts/custom/nav.js
@@ -3,12 +3,12 @@
         var nav = $('#navigation');
         var navButton = $('#globalNavButton');
         if (!nav.hasClass('open')) {
-            nav.slideDown('fast');
+            nav.slideDown();
             nav.addClass('open');
             navButton.addClass('open');
 
         } else {
-            nav.slideUp('fast');
+            nav.slideUp();
             nav.removeClass('open');
             navButton.removeClass('open');
         }

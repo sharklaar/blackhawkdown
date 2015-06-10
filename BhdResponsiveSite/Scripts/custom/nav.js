b01f9abd5aa9a4137e4f35c6e6ba0d11e7b644c4
Marc <marc@blackhawkdown.org.uk>
2015-06-10 22:09:19 +0100

    funkified nav lines

--- BhdResponsiveSite/Scripts/custom/nav.js
+++ BhdResponsiveSite/Scripts/custom/nav.js
@@ -1,12 +1,16 @@
 ﻿$(document).ready(function () {
     $('#globalNavButton').click(function () {
         var nav = $('#navigation');
+        var navButton = $('#globalNavButton');
         if (!nav.hasClass('open')) {
             nav.slideDown('fast');
             nav.addClass('open');
+            navButton.addClass('open');
+
         } else {
             nav.slideUp('fast');
             nav.removeClass('open');
-        }                
+            navButton.removeClass('open');
+        }
     })
 })
